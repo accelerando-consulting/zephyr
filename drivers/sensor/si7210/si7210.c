50cf62d43b627e22f4064a30e711ffe1704d0e87
Christopher Biggs <unixbigot@pobox.com>
2020-09-07 11:27:01 +1000

    Add silabs si7210 hall-effect sensor driver

---
+++ drivers/sensor/si7210/si7210.c
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2019 Accelerando
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT silabs_si7210
+
+#include <device.h>
+#include <drivers/i2c.h>
+#include <drivers/sensor.h>
+#include <logging/log.h>
+
+#include "si7210.h"
+
+#define SIGN_BIT_MASK 0x7F
+
+LOG_MODULE_REGISTER(si7210, CONFIG_SENSOR_LOG_LEVEL);
+
+struct si7210_data {
+	struct device *i2c_dev;
+	uint16_t temperature;
+	uint16_t field;
+};
+
+static int si7210_reg_read(struct si7210_data *drv_data, uint8_t reg,
+			     uint8_t *val)
+{
+	LOG_INF("si7210_reg_read 0x%x\n", (int)reg);
+
+	if (i2c_reg_read_byte(drv_data->i2c_dev,
+		DT_INST_REG_ADDR(0), reg, val)) {
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int si7210_reg_write(struct si7210_data *drv_data, uint8_t reg,
+			      uint8_t val)
+{
+	LOG_INF("si7210_reg_write 0x%x 0x%x\n", (int)reg, (int)val);
+	return i2c_reg_write_byte(drv_data->i2c_dev,
+		DT_INST_REG_ADDR(0), reg, val);
+}
+
+static int si7210_set_oneshot(struct si7210_data *drv_data, bool oneshot) 
+{
+	uint8_t ctrl;
+
+	if (si7210_reg_read(drv_data, SI7210_REG_CTRL, &ctrl) != 0) {
+		return -EIO;
+	}
+
+	if (oneshot) {
+		ctrl = ctrl | (1<<SI7210_CTRL_BIT_ONEBURST);
+	}
+	else {
+		ctrl = ctrl & ~(1<<SI7210_CTRL_BIT_ONEBURST);
+	}
+	ctrl = ctrl & ~(1<<SI7210_CTRL_BIT_STOP);
+
+	if (si7210_reg_write(drv_data, SI7210_REG_CTRL, ctrl) != 0) {
+		return -EIO;
+	}
+
+	return 0;
+}
+
+static int si7210_sample_fetch(struct device *dev, enum sensor_channel chan)
+{
+	struct si7210_data *drv_data = dev->data;
+
+	int retval;
+	uint8_t dspsigm;
+	uint8_t dspsigl;
+
+	if (si7210_set_oneshot(drv_data, true) != 0) {
+		return -EIO;
+	}
+	if (si7210_reg_write(drv_data, SI7210_REG_SIG_SEL, (chan==SENSOR_CHAN_AMBIENT_TEMP)?1:0) != 0) {
+		return -EIO;
+	}
+
+	retval = si7210_reg_read(drv_data, SI7210_REG_SIG_HIGH,
+				 &dspsigm);
+	retval += si7210_reg_read(drv_data, SI7210_REG_SIG_LOW,
+				  &dspsigl);
+
+	if (chan == SENSOR_CHAN_MAGN_Z) {
+		drv_data->field = (((dspsigm&0x7f)<<8)|(dspsigl&0xFF))-16384;
+	}
+	else if (chan == SENSOR_CHAN_AMBIENT_TEMP) {
+		if (retval == 0) {
+			drv_data->temperature = (256 * (dspsigm & SIGN_BIT_MASK))
+				+ dspsigl;
+		} else {
+			LOG_ERR("Read register err");
+		}
+	}
+	else {
+		retval = -ENOTSUP;
+	}
+	LOG_DBG("Sample_fetch retval: %d", retval);
+
+	return retval;
+}
+
+static int si7210_channel_get(struct device *dev, enum sensor_channel chan,
+			      struct sensor_value *val)
+{
+	struct si7210_data *drv_data = dev->data;
+	int32_t uval;
+
+	if (chan == SENSOR_CHAN_MAGN_Z) {
+		// 1 Gauss (cgs) == 0.0001 Tesla (SI mks)
+		// 1 Gauss = 0.1mT (10mT/G)
+		// mT = sensorvalue * scale
+		uval = drv_data->field * 1000000 / SI7210_SCALE_DIVISOR_COARSE;
+		val->val1 = uval / 1000000;
+		val->val2 = uval % 1000000;
+		LOG_DBG("Field = val1:%d, val2:%d", val->val1, val->val2);
+	}
+	else if (chan == SENSOR_CHAN_AMBIENT_TEMP) {
+		uval = ((55 * 160) + (drv_data->temperature - 16384)) >> 4;
+		val->val1 = uval / 10;
+		val->val2 = (uval % 10) * 100000;
+
+		LOG_DBG("Temperature = val1:%d, val2:%d", val->val1, val->val2);
+
+	} else {
+		return -ENOTSUP;
+	}
+	return 0;
+}
+
+static const struct sensor_driver_api si7210_api = {
+	.sample_fetch = &si7210_sample_fetch,
+	.channel_get = &si7210_channel_get,
+};
+
+static int si7210_chip_init(struct device *dev)
+{
+	struct si7210_data *drv_data = dev->data;
+	uint8_t value;
+
+	drv_data->i2c_dev = device_get_binding(
+		DT_INST_BUS_LABEL(0));
+
+	if (!drv_data->i2c_dev) {
+		LOG_ERR("Failed to get pointer to %s device!",
+			DT_INST_BUS_LABEL(0));
+		return -EINVAL;
+	}
+
+	if (si7210_reg_read(drv_data, SI7210_REG_CHIP_INFO,
+		&value) != 0) {
+		LOG_ERR("Chip info read failed");
+		return -EIO;
+	}
+
+	if ((value >> 4) != SI7210_CHIP_ID_VALUE) {
+		LOG_ERR("Bad chip id 0x%x", value);
+		return -ENOTSUP;
+	}
+
+	LOG_INF("si7210_chip_init success\n");
+	return 0;
+}
+
+static int si7210_init(struct device *dev)
+{
+	LOG_INF("si7210_init\n");
+	
+	if (si7210_chip_init(dev) < 0) {
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static struct si7210_data si_data;
+
+DEVICE_AND_API_INIT(si7210, DT_INST_LABEL(0), si7210_init,
+	&si_data, NULL, POST_KERNEL, CONFIG_SENSOR_INIT_PRIORITY, &si7210_api);

3ea65082795ea431bc21ace4648005b6a7d6c3c7
Christopher Biggs <unixbigot@pobox.com>
2022-02-09 16:30:25 +1000

    Dragino lsn50 peripherals

--- boards/arm/dragino_lsn50/board.cmake
+++ boards/arm/dragino_lsn50/board.cmake
@@ -1,6 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(stm32flash "--reset")
 board_runner_args(jlink "--device=STM32L072CZ" "--speed=4000")
 
+include(${ZEPHYR_BASE}/boards/common/stm32flash.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)

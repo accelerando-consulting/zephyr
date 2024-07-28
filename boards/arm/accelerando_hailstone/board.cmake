7d9753622dae8ab6485b0645d171158e71914267
Christopher Biggs <christopher@accelerando.com.au>
2024-07-28 12:13:16 +1000

    Accelerando hailstone and corinda updates

--- boards/arm/accelerando_hailstone/board.cmake
+++ boards/arm/accelerando_hailstone/board.cmake
@@ -1,7 +1,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
+board_runner_args(nrfjprog "--nrf-family=NRF52")
 board_runner_args(jlink "--device=nrf52" "--speed=4000")
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+board_runner_args(pyocd "--target=nrf52840" "--frequency=4000000")
+board_runner_args(uf2 "--board-id=hailstone")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
--- boards/arm/accelerando_hailstone/pre_dt_board.cmake
+++ boards/arm/accelerando_hailstone/pre_dt_board.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Linaro Limited
+# Copyright (c) 2024 Christopher Biggs
 # SPDX-License-Identifier: Apache-2.0
 
 # Suppress "unique_unit_address_if_enabled" to handle the following overlaps:
